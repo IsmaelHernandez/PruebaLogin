c5baf4ecd0969b112ea53d92d027f2e5d28c7699
IsmaelChampy <ismael.hernandez@magnotechnology.com>
2023-08-02 23:47:54 -0600

    corregi-lista-de-imagenes

--- src/components/CardImage.jsx
+++ src/components/CardImage.jsx
@@ -2,12 +2,9 @@ import "../css/cardimage.css";
 
 const CardImage = ({img}) => {
   return (
-      <div className="containerCard">
         <div className="card">
          <img className="img-fixed-size" src={img.url} alt="" />
         </div>
-      </div>
-    
   );
 };
 
--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -22,7 +22,6 @@ const Dashboard = ({ user, onLogout }) => {
 
   return (
     <>
-      <div className="">
         <div className="containerSeccion1">
           <div className="itemSeccion1">
             <h1 className="title1">Bienvenido, {user.username}!</h1>
@@ -32,21 +31,18 @@ const Dashboard = ({ user, onLogout }) => {
             </button>
           </div>
         </div>
-        <div className="containerSeccion2">
-          <div className="itemSeccion2">
-            
-          </div>
+        <br />
+        <div>
+          <Clock />
         </div>
         <br />
-        <br />
-        <Clock />
-        <br />
-        <hr />
         <h3 className="title1">Lista de Imagenes Consumidas por el Endpoint</h3>
-        {images?.map((img) => (
-          <CardImage img={img} key={`${img.id}`} />
-        ))}
-      </div>
+        <div className="containerSeccion2">
+          {images?.map((img) => (
+            <CardImage img={img} key={`${img.id}`} />
+          ))}
+        </div>
+        
     </>
   );
 };
